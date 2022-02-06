f9cd2cf8b6e29ce7e3ad96e829f245e943abf3e5
JosephLiCodes <69280155+JosephLiCodes@users.noreply.github.com>
2022-02-05 16:55:21 -0800

    fix models, add basic validator, create views

--- spotter/spotter_app/models.py
+++ spotter/spotter_app/models.py
@@ -1,53 +1,67 @@
 from django.db import models
-from django.utils.text import slugify
-from django.contrib.auth import get_user_model
-from django.urls import reverse
+from sqlalchemy import ForeignKey
+from django.contrib import messages
+class UserManager(models.Manager):
+    def basic_validator(self,post_data):
+        errors = {}
+        if len(post_data["username_input"]) >15:
+            errors["username_input"] = "Username Input should be 15 characters or less"
+        elif len(post_data["username_input"]) <2:
+            errors["username_input"] = "Username Input should be longer than 1 character"    
+        return errors
 
-User = get_user_model()
-
-# Create your models here.
-
-class Person(models.Model):
-    id = models.IntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    username = models.CharField(max_length=40, blank=False)
-    slug = slug = models.SlugField(max_length=400, unique=True, blank = True)
+class User(models.Model):
+    objects = UserManager()
+    username = models.CharField(max_length = 15)
+    name = models.TextField()
     bio = models.TextField()
-    realname = models.CharField(max_length=30, blank=True)
-    #upvotes = models.IntegerField(default) ?? necessary?
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.username)
-        super(Post, self).save(*args, **kwargs)
+    password = models.TextField()
+    created = models.DateTimeField(auto_now = True)
+    updated = models.DateTimeField(auto_now = True)
 
 class Post(models.Model):
-    id = models.IntegerField()
-    title = models.CharField(max_length = 400)
-    slug = models.SlugField(unique=True, max_length=255)
+    title = models.TextField()
     content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    author = models.TextField()
-    
+    author = models.ForeignKey(User,related_name="post_author",on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now = True)
+    updated = models.DateTimeField(auto_now = True)
+    reps = models.IntegerField(default=0)
 
-    def get_absolute_url(self):
-        return reverse('blog_post_detail', args=[self.slug])
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super(Post, self).save(*args, **kwargs)
-
-class Comment(models.Model):
-    id = models.IntegerField()
-    post = models.ForeignKey(Post, on_delete=models.SET_NULL)
-    timeCreated = models.TimeField(auto_now=True, auto_now_add=True)
-    user = models.ForeignKey(Person, on_delete=models.SET_NULL)
+class Comments(models.Model):
     content = models.TextField()
-    #reps = models.IntegerField()
+    author = models.ForeignKey(User,related_name="comment_author",on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now = True)
+    updated = models.DateTimeField(auto_now = True)
+    reps = models.IntegerField(default=0)
 
-class Reps(models.Model):
-    id = models.IntegerField()
-    post = models.ForeignKey(Post, on_delete=models.SET_NULL)
 
-    
+# Create your models here.
+# User {
+#   - posts (one to many) posts = models.ForeignKey(User, related_name=, on_delete=models.CASCADE)
+#   - comments (one to many)
+#   - username
+#   - name
+#   - bio
+#   - password
+#   - created
+#   - updated
+# }
+
+# Post {
+#     title
+#     content
+#     author (foreign key -> User) author = models.(foreign_key=, related_name=)
+#     created
+#     updated = models.DateTimeField(auto_now=True)
+#     comments (one to many)
+#     reps
+# }
+
+# Comment {
+#     content
+#     author (foreign key -> User)
+#     created
+#     updated = models.DateTimeField(auto_now=True)
+#     reps
+# }
+
--- spotter/spotter_app/views.py
+++ spotter/spotter_app/views.py
@@ -1,6 +1,7 @@
 from http.client import HTTPResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import *
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 def index(request):
     test_arr = ['<div class="test" message="# ' + str(i) + ' generated with Django templates!"></div>' for i in range(1, 4)]
@@ -12,7 +13,17 @@ def index(request):
 def login(request):
     return render(request, 'login.html')
 
+@csrf_exempt
 def register(request):
+    if request.method == "POST":
+        errors = User.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/')
+        User.objects.create(username = request.POST["username_input"], bio =  request.POST["bio_input"], name =  request.POST["realname_input"] )
+        print("hahaha")
+
     return render(request, 'register.html')
 
 def user(request, username):
